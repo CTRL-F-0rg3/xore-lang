d4a8837f9632a6d299411149ab9050247040ce8b
ctrl-f-0-rg3 <ctrlf0rge@protonmail.com>
2026-04-23 17:09:30 +0200

    0.0.0.3

--- src/lexer.rs
+++ src/lexer.rs
@@ -55,141 +55,166 @@ pub enum Token {
 
 
 
+#[derive(Debug, PartialEq, Clone)]
+pub struct Pos {
+    pub line: usize,
+    pub col: usize,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum LexerError {
+    UnexpectedChar(char, Pos),
+    UnterminatedString(Pos),
+    InvalidNumber(Pos),
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum Token {
+    // ... Twoje poprzednie tokeny ...
+    Error(LexerError),
+    Identifier(String),
+    IntLiteral(i128),
+    FloatLiteral(f64),
+    StrLiteral(String),
+    EOF,
+}
+
 pub struct Lexer<'a> {
     input: Peekable<Chars<'a>>,
+    line: usize,
+    col: usize,
 }
 
 impl<'a> Lexer<'a> {
     pub fn new(input: &'a str) -> Self {
-        Self { input: input.chars().peekable() }
+        Self {
+            input: input.chars().peekable(),
+            line: 1,
+            col: 1,
+        }
+    }
+
+    fn next_char(&mut self) -> Option<char> {
+        let ch = self.input.next()?;
+        if ch == '\n' {
+            self.line += 1;
+            self.col = 1;
+        } else {
+            self.col += 1;
+        }
+        Some(ch)
+    }
+
+    fn peek_char(&mut self) -> Option<&char> {
+        self.input.peek()
     }
 
     pub fn next_token(&mut self) -> Token {
-        self.skip_whitespace();
+        self.skip_whitespace_and_comments();
 
-        match self.input.next() {
-            Some('+') => Token::ADD,
-            Some('*') => Token::STAR,
-            Some('/') => Token::SLASH,
-            Some('%') => Token::PERCENT,
-            Some(',') => Token::COMMA,
-            Some('.') => Token::DOT,
-            Some(':') => Token::COLON,
-            Some(';') => Token::SEMICOLON,
-            Some('(') => Token::L_PAREN,
-            Some(')') => Token::R_PAREN,
-            Some('{') => Token::L_BRACE,
-            Some('}') => Token::R_BRACE,
-            Some('[') => Token::L_BRACKET,
-            Some(']') => Token::R_BRACKET,
+        let pos = Pos { line: self.line, col: self.col };
+        
+        let ch = match self.next_char() {
+            Some(c) => c,
+            None => return Token::EOF,
+        };
 
-            Some('-') => {
-                if self.peek_is('>') { Token::ARROW } 
-                else { Token::MINUS }
-            }
-            Some('=') => {
-                if self.peek_is('=') { Token::EQ } 
-                else { Token::ASSIGN }
-            }
-            Some('!') => {
-                if self.peek_is('=') { Token::NOT_EQ } 
-                else { Token::NOT }
-            }
-            Some('<') => {
-                if self.peek_is('=') { Token::LTE } 
-                else { Token::LT }
-            }
-            Some('>') => {
-                if self.peek_is('=') { Token::GTE } 
-                else { Token::GT }
-            }
-            Some('&') => {
-                if self.peek_is('&') { Token::AND } 
-                else { panic!("Expected &&") }
-            }
-            Some('|') => {
-                if self.peek_is('|') { Token::OR } 
-                else { panic!("Expected ||") }
-            }
+        match ch {
+            '+' => Token::ADD,
+            '*' => Token::STAR,
+            '/' => Token::SLASH,
+            '%' => Token::PERCENT,
+            '(' => Token::L_PAREN,
+            ')' => Token::R_PAREN,
+            '{' => Token::L_BRACE,
+            '}' => Token::R_BRACE,
+            ',' => Token::COMMA,
+            ';' => Token::SEMICOLON,
 
-            Some('"') => self.lex_string(),
+            '-' => if self.consume_if('>') { Token::ARROW } else { Token::MINUS },
+            '=' => if self.consume_if('=') { Token::EQ } else { Token::ASSIGN },
+            '!' => if self.consume_if('=') { Token::NOT_EQ } else { Token::NOT },
+            '<' => if self.consume_if('=') { Token::LTE } else { Token::LT },
+            '>' => if self.consume_if('=') { Token::GTE } else { Token::GT },
+            '&' => if self.consume_if('&') { Token::AND } else { Token::Error(LexerError::UnexpectedChar('&', pos)) },
+            '|' => if self.consume_if('|') { Token::OR } else { Token::Error(LexerError::UnexpectedChar('|', pos)) },
 
-            Some(c) if c.is_alphabetic() || c == '_' => self.lex_identifier(c),
-            Some(c) if c.is_numeric() => self.lex_number(c),
+            '"' => self.lex_string(pos),
 
-            None => Token::EOF,
-            _ => panic!("Unexpected character"),
+            c if c.is_ascii_alphabetic() || c == '_' => self.lex_identifier(c),
+            c if c.is_ascii_digit() => self.lex_number(c, pos),
+
+            _ => Token::Error(LexerError::UnexpectedChar(ch, pos)),
         }
     }
 
-    fn lex_identifier(&mut self, first: char) -> Token {
-        let mut id = first.to_string();
-        while let Some(&c) = self.input.peek() {
-            if c.is_alphanumeric() || c == '_' {
-                id.push(self.input.next().unwrap());
+    fn lex_number(&mut self, first: char, pos: Pos) -> Token {
+        let mut s = first.to_string();
+        let mut is_float = false;
+
+        while let Some(&c) = self.peek_char() {
+            if c.is_ascii_digit() || c == '_' {
+                if c != '_' { s.push(c); }
+                self.next_char();
+            } else if c == '.' && !is_float {
+                is_float = true;
+                s.push(self.next_char().unwrap());
+            } else {
+                break;
+            }
+        }
+
+        if is_float {
+            s.parse::<f64>().map(Token::FloatLiteral).unwrap_or(Token::Error(LexerError::InvalidNumber(pos)))
+        } else {
+            s.parse::<i128>().map(Token::IntLiteral).unwrap_or(Token::Error(LexerError::InvalidNumber(pos)))
+        }
+    }
+
+    fn lex_string(&mut self, start_pos: Pos) -> Token {
+        let mut s = String::new();
+        while let Some(ch) = self.next_char() {
+            match ch {
+                '"' => return Token::StrLiteral(s),
+                '\\' => match self.next_char() {
+                    Some('n') => s.push('\n'),
+                    Some('t') => s.push('\t'),
+                    Some('\\') => s.push('\\'),
+                    Some('"') => s.push('"'),
+                    Some(c) => s.push(c),
+                    None => return Token::Error(LexerError::UnterminatedString(start_pos)),
+                },
+                _ => s.push(ch),
+            }
+        }
+        Token::Error(LexerError::UnterminatedString(start_pos))
+    }
+
+    fn skip_whitespace_and_comments(&mut self) {
+        while let Some(&c) = self.peek_char() {
+            if c.is_whitespace() {
+                self.next_char();
+            } else if c == '/' {
+                // Podglądamy czy to komentarz bez zjadania '/'
+                if self.nth_char_is(1, '/') {
+                    while let Some(nc) = self.next_char() {
+                        if nc == '\n' { break; }
+                    }
+                } else { break; }
             } else { break; }
         }
-
-        match id.as_str() {
-            "i32" => Token::I32,
-            "i64" => Token::I64,
-            "i128" => Token::I128,
-            "i256" => Token::I256,
-            "String" => Token::STRING,
-            "str" => Token::S_STRING,
-            "bool" => Token::BOOL,
-            "public" => Token::PUBLIC,
-            "private" => Token::PRIVATE,
-            "fn" => Token::FN,
-            "enum" => Token::ENUM,
-            "struct" => Token::STRUCT,
-            "if" => Token::IF,
-            "else" => Token::ELSE,
-            "switch" => Token::SWITCH,
-            "case" => Token::CASE,
-            "default" => Token::DEFAULT,
-            "for" => Token::FOR,
-            "while" => Token::WHILE,
-            "return" => Token::RETURN,
-            "let" => Token::LET,
-            "mut" => Token::MUT,
-            "true" => Token::BoolLiteral(true),
-            "false" => Token::BoolLiteral(false),
-            _ => Token::Identifier(id),
-        }
     }
 
-    fn lex_number(&mut self, first: char) -> Token {
-        let mut num = first.to_string();
-        while let Some(&c) = self.input.peek() {
-            if c.is_numeric() { num.push(self.input.next().unwrap()); } 
-            else { break; }
-        }
-        Token::IntLiteral(num.parse().unwrap())
-    }
-
-    fn lex_string(&mut self) -> Token {
-        let mut s = String::new();
-        while let Some(c) = self.input.next() {
-            if c == '"' { break; }
-            s.push(c);
-        }
-        Token::StrLiteral(s)
-    }
-
-    fn skip_whitespace(&mut self) {
-        while let Some(&c) = self.input.peek() {
-            if c.is_whitespace() { self.input.next(); } 
-            else { break; }
-        }
-    }
-
-    fn peek_is(&mut self, expected: char) -> bool {
-        if self.input.peek() == Some(&expected) {
-            self.input.next();
+    fn consume_if(&mut self, expected: char) -> bool {
+        if self.peek_char() == Some(&expected) {
+            self.next_char();
             true
         } else {
             false
         }
+    }
+
+    fn nth_char_is(&self, n: usize, expected: char) -> bool {
+        self.input.clone().nth(n) == Some(expected)
     }
 }
